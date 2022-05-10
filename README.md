7fbaffe136bdedb938f5fd5969de272d00267dc3
Julian B <julianb@Julians-Mini.nyc.rr.com>
2022-05-10 00:17:33 -0400

    first commit, lets go

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# personal_website
+# personal_website 
+
+## Goals
+
+* banner of socials
+
+* react router to alternate pages, pages like a blog, self made games, and to be determined
+
+* elevator pitch
+
+* online resume??? It has your address though dude? bad idea
+
+* need to determine what else to add to website
---
+++ julian_website/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+ReactDOM.render(
+  <><div id="menu">Buttons go here, like
+    <a href="https://media.giphy.com/media/3oEjHAUOqG3lSS0f1C/giphy.gif"> Github accomplishments</a>
+  </div>
+  <div id="elevator pitch">
+    <p>Hello, one and all! to the main page for your local clown show. Here we show a work in progress
+      as the clown in residence tries in a vain attempt to keep up his programming habits up to date!
+    </p>
+  </div>
+  <div id="socials">
+    <h3>Linkedin</h3>
+    <h3>github</h3>
+    <h3>twitter | need to figure out what to do with twitter, maybe idea dumping ground?!</h3>
+  </div>
+  </>
+  ,document.getElementById("root")
+
+)
+
