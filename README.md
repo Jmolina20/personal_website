0b67f1c3e31a008788e36ff21cc00de88bec52b8
Jmolina20 <julianmolina2011@gmail.com>
2022-05-23 14:25:24 -0400

    small changes

--- README.md
+++ README.md
@@ -11,3 +11,6 @@
 * online resume??? It has your address though dude? bad idea
 
 * need to determine what else to add to website
+
+
+* which twitter should i link? what kind of things are expected out of a professional programmer's twitter??? I like this lofi programming music to focus?? shoutout to repos and side projects I think are cool??
--- julian_website/src/index.js
+++ julian_website/src/index.js
@@ -11,11 +11,17 @@ ReactDOM.render(
     <p>Hello, one and all! to the main page for your local clown show. Here we show a work in progress
       as the clown in residence tries in a vain attempt to keep up his programming habits up to date!
     </p>
+    <ul> List of qualities
+      <li>I can speak english</li>
+      <li>I can breathe somewhat properly</li>
+      <li>I have an uncanny ability to always being open to taking home leftovers</li>
+    </ul>
+
   </div>
   <div id="socials">
-    <h3>Linkedin</h3>
-    <h3>github</h3>
-    <h3>twitter | need to figure out what to do with twitter, maybe idea dumping ground?!</h3>
+    <h3><a href="">Linkedin</a></h3>
+    <h3><a href="https://github.com/Jmolina20">Github</a></h3>
+    <h3>twitter | need to figure out what to do with twitter, maybe idea dumping ground?! DONE LMAO</h3>
   </div>
   </>
   ,document.getElementById("root")
